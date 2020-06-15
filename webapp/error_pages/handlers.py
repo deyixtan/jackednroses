e204d65ab497a0a9918a5281516cef6db68f7c55
Deyi Tan <deyixtan@gmail.com>
2020-06-15 17:02:43 +0800

    Added db rollback for internal server error

--- webapp/error_pages/handlers.py
+++ webapp/error_pages/handlers.py
@@ -1,16 +1,19 @@
 # webapp/error_pages/handlers.py
+# naming reference from https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 from flask import render_template, Blueprint
+from webapp import db
 
 error_pages = Blueprint("error_pages", __name__)
 
 @error_pages.app_errorhandler(403)
-def error_403(error):
+def forbidden(error):
     return (render_template("error_pages/403.html"), 403)
 
 @error_pages.app_errorhandler(404)
-def error_404(error):
+def not_found(error):
     return (render_template("error_pages/404.html"), 404)
 
 @error_pages.app_errorhandler(500)
-def error_500(error):
+def internal_server_error(error):
+    db.session.rollback()
     return (render_template("error_pages/500.html"), 500)
