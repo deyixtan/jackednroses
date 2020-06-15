012396fd82cad5090f47f08f3f3d3216df7c1276
Deyi Tan <deyixtan@gmail.com>
2020-06-15 16:28:56 +0800

    Added shell context debugging capability

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
 # run.py
-from webapp import app
+from webapp import app, db
+from webapp.models import User, Module, Enrolled
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "User": User,
+        "Module": Module,
+        "Enrolled": Enrolled
+    }
 
 # Python script entry point
 if __name__ == "__main__":
