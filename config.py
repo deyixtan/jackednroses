b20ffd9498ecb4b04feda1586131d0fb4b32a611
Nathaniel Ho <nathaniel_ho1997@hotmail.com>
2020-07-22 17:27:51 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "jackednroses"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or "sqlite:///" + os.path.join(basedir, "data.sqlite")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or "sqlite:///" + os.path.join(basedir, "data.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
