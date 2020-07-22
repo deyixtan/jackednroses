e6b3027abad431a122888c31f7b0de27b3bb62d8
Nathaniel Ho <nathaniel_ho1997@hotmail.com>
2020-07-22 16:37:29 +0800

    Addtional config settings for deployment

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "jackednroses"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or "sqlite:///" + os.path.join(basedir, "data.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
