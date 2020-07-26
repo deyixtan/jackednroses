a8eba9111cb52619d49efd4345d7e5d7db513520
Nathaniel Ho <nathaniel_ho1997@hotmail.com>
2020-07-26 19:52:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 from datetime import datetime, timedelta
 from webapp import create_app, db
-from webapp.models import User, Module, Enrolled, Announcement, Task, Exam, ExamDetails, UserDetails, UhmsMessages
+from webapp.models import ModuleUserMap, ModulePluginMap, ModuleTaskUserMap, HostelRoomUserMap, User, UserProfile, Module, ModuleAnnouncement, ModuleTask, Plugin, Hostel, HostelRoom, HostelApplication, HostelMessage
 from config import Config
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import FlushError
@@ -24,143 +24,138 @@ class UserModelCase(unittest.TestCase):
         db.drop_all()
         self.app_context.pop()
     
-    def test_announcement_creation(self):
-        m = Module(code="CS2100", academic_year="2019/2020", semester=1)
-        db.session.add(m)
-        db.session.commit()
-        a = Announcement(module_id=m.id, title="Announcement Title")
-        db.session.add(a)
-        db.session.commit()
-        self.assertEqual(Announcement.query.filter_by(module_id=m.id, title=a.title).first(), a)
-
-    def test_enrollment_integrity_check(self):
-        m = Module(code="CS2100", academic_year="2019/2020", semester="1")
-        u = User(nusnetid="E1234567")
-        db.session.add(m)
-        db.session.add(u)
-        db.session.commit()
-        e = Enrolled(nusnetid=u.nusnetid, module_id=m.id)
-        db.session.add(e)
-        db.session.commit()
-        e2 = Enrolled(nusnetid=u.nusnetid, module_id=m.id)
-        with self.assertRaises(Exception) as context:
-            db.session.add(e2)
-            db.session.commit()
-        self.assertEqual(type(context.exception), FlushError)
-
-    def test_exam_creation(self):
-        m = Module(code="CS2100", academic_year="2019/2020", semester="1")
-        u = User(nusnetid="E1234567")
-        db.session.add(m)
-        db.session.add(u)
-        db.session.commit()
-        e = Exam(module_id=m.id, examname="Exam Name")
-        db.session.add(e)
-        db.session.commit()
-        ed = ExamDetails(nusnetid=u.nusnetid, exam_id=e.id)
-        db.session.add(ed)
-        db.session.commit()
-        self.assertEqual(Exam.query.filter_by(module_id=m.id).first(), e)
-        self.assertEqual(ExamDetails.query.filter_by(nusnetid=u.nusnetid, exam_id=e.id).first(), ed)
-    
-    def test_exam_enrollment_integrity_check(self):
-        m = Module(code="CS2100", academic_year="2019/2020", semester="1")
-        u = User(nusnetid="E1234567")
-        db.session.add(m)
-        db.session.add(u)
-        db.session.commit()
-        e = Exam(module_id=m.id, examname="Exam Name")
-        db.session.add(e)
-        db.session.commit()
-        ed = ExamDetails(nusnetid=u.nusnetid, exam_id=e.id)
-        db.session.add(ed)
-        db.session.commit()
-        ed2 = ExamDetails(nusnetid=u.nusnetid, exam_id=e.id)
-        with self.assertRaises(Exception) as context:
-            db.session.add(ed2)
-            db.session.commit()
-        self.assertEqual(type(context.exception), FlushError)
-
-    def test_module_creation_and_enrolllment(self):
-        m = Module(code="CS2100", academic_year="2019/2020", semester=1)
-        db.session.add(m)
-        db.session.commit()
-        u = User(nusnetid="E1234567")
-        e = Enrolled(nusnetid=u.nusnetid)
-        e.set_module(code=m.code, academic_year=m.academic_year, semester=m.semester)
-        db.session.add(u)
-        db.session.add(e)
-        db.session.commit()
-        self.assertEqual(m.check_exist(m), m)
-        self.assertEqual(Enrolled.query.filter_by(nusnetid=u.nusnetid, module_id=m.id).first(), e)
-
-    def test_module_integrity_check(self):
-        m = Module(code="CS2100", academic_year="2019/2020", semester=1)
-        db.session.add(m)
-        db.session.commit()
-        m2 = Module(code="CS2100", academic_year="2019/2020", semester=1)
-        self.assertEqual(m2.check_exist(m2), m)
-
-    def test_task_creation(self):
-        m = Module(code="CS2100", academic_year="2019/2020", semester=1)
-        db.session.add(m)
-        db.session.commit()
-        t = Task(module_id=m.id, taskname="Task Title")
-        db.session.add(t)
-        db.session.commit()
-        self.assertEqual(Task.query.filter_by(module_id=m.id).first(), t)
-
+    #user related tests
     def test_password_hashing(self):
-        u = User(nusnetid="E1234567")
+        u = User(username="E1234567")
         u.set_password("warmthecockles")
         self.assertFalse(u.check_password("titanicSinking"))
         self.assertTrue(u.check_password("warmthecockles"))
 
-    def test_userdetails_creation(self):
-        u = User(nusnetid="E1234567")
-        db.session.add(u)
-        db.session.commit()
-        ud = UserDetails(nusnetid=u.nusnetid, nric="S1234567A", mobilenum=91234567)
-        db.session.add(ud)
-        db.session.commit()
-        self.assertEqual(UserDetails.query.filter_by(nusnetid=u.nusnetid).first().nusnetid, u.nusnetid)
-
-    def test_userdetails_update(self):
-        u = User(nusnetid="E1234567")
-        db.session.add(u)
-        db.session.commit()
-        ud = UserDetails(nusnetid=u.nusnetid, nric="S1234567A", gender="Male", marital_status="Single", nationality="Singaporean", mobilenum=91234567, homenum=61234567, address="Orchard Road Block 123 #01-01", emergencycontactname="Gill Bates", emergencycontactnum=81234567)
-        db.session.add(ud)
-        db.session.commit()
-        ud.nric = "S7654321B"
-        ud.gender = "Female"
-        ud.marital_status = "Married"
-        ud.nationality = "Australian"
-        ud.mobilenum = 97654321
-        ud.homenum = 67654321
-        ud.address = "Tuas Checkpoint #01-02"
-        ud.emergencycontactname = "Melon Musk"
-        ud.emergencycontactnum = 87654321
-        db.session.add(ud)
-        db.session.commit()
-        self.assertEqual(UserDetails.query.filter_by(nusnetid=u.nusnetid).first(), ud)
-
     def test_user_integrity_check(self):
-        u = User(nusnetid="E1234567")
+        u = User(username="E1234567")
         db.session.add(u)
         db.session.commit()
-        u2 = User(nusnetid="E1234567")
+        u2 = User(username="E1234567")
         with self.assertRaises(Exception) as context:
             db.session.add(u2)
             db.session.commit()
         self.assertEqual(type(context.exception), IntegrityError)
-        
-    def test_uhms_message_creation(self):
-        m = UhmsMessages(title="Announcement Title")
+
+    def test_userprofile_creation(self):
+        u = User(username="E1234567")
+        db.session.add(u)
+        db.session.commit()
+        ud = UserProfile(user_id=u.id, nric="S1234567A", mobile_number=91234567)
+        db.session.add(ud)
+        db.session.commit()
+        self.assertEqual(UserProfile.query.filter_by(user_id=u.id).first(), ud)
+
+    def test_userprofile_update(self):
+        u = User(username="E1234567")
+        db.session.add(u)
+        db.session.commit()
+        ud = UserProfile(user_id=u.id, nric="S1234567A", first_name="Billy", last_name="Eyelash", gender="Male", marital_status="Single", nationality="Singaporean", mobile_number=91234567, home_number=61234567, home_address="Orchard Road Block 123 #01-01", emergency_contact_name="Gill Bates", emergency_contact_number=81234567)
+        db.session.add(ud)
+        db.session.commit()
+        ud.first_name = "Billie"
+        ud.last_name = "Eilish"
+        ud.gender = "Female"
+        ud.marital_status = "Married"
+        ud.nationality = "Russian"
+        ud.mobile_number = 97654321
+        ud.home_number = 67654321
+        ud.home_address = "Tuas Checkpoint #01-02"
+        ud.emergency_contact_name = "Melon Musk"
+        ud.emergency_contact_number = 87654321
+        db.session.add(ud)
+        db.session.commit()
+        self.assertEqual(UserProfile.query.filter_by(user_id=u.id).first(), ud)
+
+    #module related tests
+    def test_module_creation_and_enrolllment(self):
+        m = Module(code="CS2100", academic_year=2019, semester=1)
+        u = User(username="E1234567")
+        db.session.add(m)
+        db.session.add(u)
+        db.session.commit()
+        e = ModuleUserMap(module_id=m.id, user_id=u.id)
+        db.session.add(e)
+        db.session.commit()
+        self.assertEqual(Module.query.filter_by(code="CS2100", academic_year=2019, semester=1).first(), m)
+        self.assertEqual(ModuleUserMap.query.filter_by(user_id=u.id, module_id=m.id).first(), e)
+
+    def test_module_integrity_check(self):
+        m = Module(code="CS2100", academic_year=2019, semester=1)
         db.session.add(m)
         db.session.commit()
-        self.assertEqual(UhmsMessages.query.filter_by(title=m.title).first(), m)
- 
+        m2 = Module(code="CS2100", academic_year=2019, semester=1)
+        self.assertEqual(m2.check_exist(m2), m)
 
+    def test_enrollment_integrity_check(self):
+        m = Module(code="CS2100", academic_year=2019, semester="1")
+        u = User(username="E1234567")
+        db.session.add(m)
+        db.session.add(u)
+        db.session.commit()
+        e = ModuleUserMap(module_id=m.id, user_id=u.id)
+        db.session.add(e)
+        db.session.commit()
+        e2 = ModuleUserMap(module_id=m.id, user_id=u.id)
+        with self.assertRaises(Exception) as context:
+            db.session.add(e2)
+            db.session.commit()
+        self.assertEqual(type(context.exception), FlushError)
+    
+    def test_announcement_creation(self):
+        m = Module(code="CS2100", academic_year=2019, semester=1)
+        db.session.add(m)
+        db.session.commit()
+        a = ModuleAnnouncement(module_id=m.id, title="Announcement Title")
+        db.session.add(a)
+        db.session.commit()
+        self.assertEqual(ModuleAnnouncement.query.filter_by(module_id=m.id, title=a.title).first(), a)
+
+    def test_task_creation(self):
+        m = Module(code="CS2100", academic_year=2019, semester=1)
+        db.session.add(m)
+        db.session.commit()
+        t = ModuleTask(module_id=m.id, title="Task Title")
+        db.session.add(t)
+        db.session.commit()
+        self.assertEqual(ModuleTask.query.filter_by(module_id=m.id).first(), t)
+
+    #hostel tests
+    def test_hostel_and_room_creation(self):
+        h = Hostel(name="Stardew Valley Hall", type="Hall")
+        h2 = Hostel(name="Harvest Moon Hall", type="Hall")
+        db.session.add(h)
+        db.session.add(h2)
+        db.session.commit()
+        r = HostelRoom(hostel_id=h.id, block="1")
+        r2 = HostelRoom(hostel_id=h2.id, block="1A")
+        db.session.add(r)
+        db.session.add(r2)
+        db.session.commit()
+        self.assertEqual(HostelRoom.query.filter_by(hostel_id=h.id, block=r.block).first(), r)
+        self.assertEqual(HostelRoom.query.filter_by(hostel_id=h2.id, block=r2.block).first(), r2)
+
+    def hostel_approval_and_message_check(self):
+        h = Hostel(name="Stardew Valley Hall", type="Hall")
+        r = HostelRoom(hostel_id=h.id, block="1")
+        msg = HostelMessage(hostel_id=h.id, title="Welcome to Stardew Valley!")
+        u = User(username="E1234567")
+        db.session.add(h)
+        db.session.add(r)
+        db.session.add(hmsg)
+        db.session.add(u)
+        db.session.commit()
+        happ = HostelApplication(user_id=u.id, hostel_id=h.id)
+        db.session.add(ha)
+        db.session.commit()
+        hmap = HostelRoomUserMap(hostel_room_id=r.id, user_id=happ.id)
+        db.session.add(hmap)
+        db.session.commit()
+        self.assertEqual(User.query.filter_by(user_id=u.id).first().hostel_room.hostel.messages, msg)
+        
 if __name__ == "__main__":
     unittest.main(verbosity=2)
