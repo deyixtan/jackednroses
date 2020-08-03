99a168e0ef1f62c7fb75edea558bb43c2e2287bb
Deyi Tan <deyixtan@gmail.com>
2020-08-03 11:59:54 +0800

    Added checks to prevent unauthorised users from gaining access to admin CP

--- webapp/admin/routes.py
+++ webapp/admin/routes.py
@@ -1,6 +1,14 @@
-from flask import render_template, request, url_for
-from flask_login import login_required
+from flask import abort, render_template, request, url_for
+from flask_login import current_user, login_required
 from webapp.admin import bp
+
+
+@bp.before_request
+def before_request():
+    if not current_user:
+        abort(404)
+    if not current_user.is_admin:
+        abort(404)
 
 
 @bp.route("/")
